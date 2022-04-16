31297f18e9a0997809833a4a97c2226fbb282b14
a1conwf <anton.kashuba@gmail.com>
2022-04-16 19:50:32 +0300

    Fix btn active class script

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    //Toggles active class while clicking on buttons, adds dynamical value to output text
+    //When clicking on btns removes active class if btn has it, if it doesn`t have it, adds active class. Also, adds dynamical value to output text.
     $("ul li button").click(function () {
-        $(this).toggleClass("active");
+        $("ul li button.active").removeClass("active");
+        $(this).addClass("active");
         const btnValue = $(this).val();
         $(".output__text").text("You have selected " + btnValue + " out of 5");
     });
